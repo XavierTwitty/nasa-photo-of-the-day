3511ee78c1cc60c9a1bafa9733285a8c26825e6e
XavierTwitty <x.twitty@gmail.com>
2021-01-07 18:56:26 -0500

    data logged in the console

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [dataOfTheDay, SetdataOfTheDay] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((response) => SetdataOfTheDay(response.data));
+    console.log("in our effect");
+  }, []);
+
+  useEffect(() => {
+    console.log("picture of the day ", dataOfTheDay);
+  }, [dataOfTheDay]);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
 }
-
 export default App;
